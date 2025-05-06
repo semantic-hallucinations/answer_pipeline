1dd4c61eea751dce3779a00d139207923479be93
Alitey <alyalu004@gmail.com>
2025-05-06 23:12:51 +0300

    rebase files from fix branch

--- src/main.py
+++ src/main.py
@@ -1,17 +1,44 @@
-from fastapi import FastAPI, Header
-from pydantic import BaseModel
+import logging
+import pathlib
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+from llama_index.core.chat_engine.types import BaseChatEngine
+from llama_index.core import Settings
+from llama_index.llms.openrouter import OpenRouter
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from fastapi import FastAPI, Depends
+
+from settings import settings
+from pipeline.dependencies import get_chat_engine
+
+
+Settings.llm = OpenRouter(
+    model=settings.get_model_name(),
+    api_key=settings.get_model_key(),
+)
+Settings.embed_model = HuggingFaceEmbedding(
+    model_name="thenlper/gte-large",
+
+)
 
 app = FastAPI()
 
 
-class TextRequest(BaseModel):
-    text: str
+def get_mock_index():
+    from llama_index.core import SimpleKeywordTableIndex, SimpleDirectoryReader
 
+    documents = SimpleDirectoryReader("data").load_data()
+    return SimpleKeywordTableIndex.from_documents(documents) 
 
-@app.post("/length/")
-async def calculate_length(
-    request: TextRequest, x_multiplier: int = Header(1, alias="multiplier")
+from pipeline.dependencies import get_index
+app.dependency_overrides[get_index] = get_mock_index
+
+@app.get("/")
+def main(
+    message: str,
+    chat_engine: BaseChatEngine = Depends(get_chat_engine)
 ):
-    length = len(request.text)
-    result = length * x_multiplier
-    return {"length": length, "result": result}
+    return chat_engine.chat(message) 
---
+++ src/settings.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os 
+
+class ModelSettings(BaseSettings):
+    MODEL_NAME: str
+    API_CLIENT_TOKEN: str
+    QDRANT_ADDRESS: str
+    QDRANT_PORT: str
+    QDRANT_COLLECTION: str
+
+    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../.env"), extra="ignore")
+
+    def get_model_name(self):
+        return self.MODEL_NAME
+    
+    def get_model_key(self):
+        return self.API_CLIENT_TOKEN
+    
+    def get_qdrant_url(self):
+        return f'http://{self.QDRANT_ADDRESS}:{self.QDRANT_PORT}'
+    
+    def get_qdrant_collection(self):
+        return self.QDRANT_COLLECTION   
+    
+settings = ModelSettings()
